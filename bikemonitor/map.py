1a1a1661c2e4a3a3907aa8d2233a36158b7feba4
Kai Luedemann <kluedema@ualberta.ca>
2022-05-26 15:55:54 -0600

    Added location data to map

--- bikemonitor/map.py
+++ bikemonitor/map.py
@@ -1,13 +1,42 @@
+from curses import raw
 import functools
 
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for
+    Blueprint, flash, g, redirect, render_template, request, session, url_for, current_app
 )
 from bikemonitor.db import get_db
+from math import sqrt
 
 bp = Blueprint('map', __name__)
 
 @bp.route("/")
 def index():
-    return render_template("index.html")
+    db = get_db()
+    with current_app.open_resource('query.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+    
+    # db = get_db()
+    # raw_data = db.execute('SELECT user_id, time_stamp, trip_id, latitude, longitude FROM locations ORDER BY user_id, time_stamp;').fetchall()
+    
+    # data = []
+    # current = raw_data[0]
+    # for i in range(1, len(raw_data)):
+    #     prev = current
+    #     current = raw_data[i]
+    #     if (current[0] == prev[0] and current[2] == prev[2]):
+    #         average = db.execute(
+    #             "SELECT AVG(x_accel * x_accel + y_accel * y_accel + z_accel * z_accel) FROM accelerometer WHERE user_id=? AND time_stamp<=? AND time_stamp>=?", 
+    #             [current[0], current[1], prev[1]]
+    #         ).fetchone()[0]
+    #         average = min(sqrt(average), 20)
+    #         color = int(average * 510 // 20)
+    #         red = 255
+    #         green = 255
+    #         if (color > 255):
+    #             green = 510 - color
+    #         else:
+    #             red = 510 - color
+    #         # print(average, color, red, green)
+    #         data.append({'points': [prev[3:5], current[3:5]], 'color': f'#{red:02X}{green:02X}00'})
+    return render_template("index.html", data=data)
 
---
+++ bikemonitor/query.sql
@@ -0,0 +1,16 @@
+-- Return a segment of a trip with the average squared magnitude of acceleration over that segment
+-- Also include the start and end points of that segment
+SELECT lat1, lon1, lat2, lon2, AVG(x_accel * x_accel + y_accel * y_accel + z_accel * z_accel)
+FROM (
+    -- For each location data instance, find the previous coordinates and time_stamp within the same trip
+    SELECT l.user_id as uid, l.time_stamp as ts2, m.time_stamp as ts1, l.latitude as lat2, l.longitude as lon2, m.latitude as lat1, m.longitude as lon1
+    FROM locations l, locations m
+    WHERE l.user_id = m.user_id AND l.trip_id = m.trip_id AND m.time_stamp = (
+        -- Find previous time_stamp
+        SELECT MAX(time_stamp) 
+        FROM locations n 
+        WHERE n.user_id = l.user_id AND n.trip_id = l.trip_id AND n.time_stamp < l.time_stamp
+    )
+) b, accelerometer a
+WHERE b.uid = a.user_id AND a.time_stamp <= b.ts2 and a.time_stamp >= b.ts1
+GROUP BY b.uid, b.ts2
